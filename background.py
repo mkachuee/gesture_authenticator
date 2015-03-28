ed931c64e967adde80a68d16a9a3feeef76ed33c
MohamadKachuee <m.kachuee@gmail.com>
2015-03-28 21:38:40 +0430

    Body of background remover added

---
+++ background.py
@@ -0,0 +1,17 @@
+"""
+This module implements phase1 of the project
+"""
+# imports
+import pdb
+
+import numpy as np
+import cv2
+
+# options
+
+def remove_background(frames_background, frame_input):
+    """
+    This function removes background from the frame_input
+    """
+    return frame_input
+
