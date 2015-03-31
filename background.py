0640fd9eb66240b21d8bb7d678a0d88e0d5f36f0
MohamadKachuee <m.kachuee@gmail.com>
2015-03-31 12:17:44 +0430

    Dilation process added.

--- background.py
+++ background.py
@@ -22,23 +22,33 @@ def remove_background(frame_background, frame_input):
     ret, frame_output_5 = cv2.threshold(frame_output_4, 64, 255, cv2.THRESH_BINARY)
     frame_output_6 = np.tile(frame_output_5.transpose()/255, (3, 1, 1)).transpose() * frame_input
     
+
     # extra precessings
-    frame_output_gaussian = cv2.GaussianBlur(frame_output_6, (3, 3), 0, 0)
+    #frame_output_gaussian = cv2.GaussianBlur(frame_output_6, (3, 3), 0, 0)
+    
     # median of image to remove salt and papper noise
     frame_median = cv2.medianBlur(frame_output_6, 3)
+    
+    # crop image
     median_gray = cv2.cvtColor(frame_median, cv2.COLOR_BGR2GRAY)
     ind = np.nonzero(median_gray)
-    
-    
     frame_cropped = np.array([[0, 0],[0, 0]])
     if len(ind[0]) !=0:
-        #frame_cropped = median_gray[ind[0][0]:ind[0][-1], ind[1][0]:ind[1][-1]]
         frame_cropped = frame_output_6[np.min(ind[0]):np.max(ind[0]), 
             np.min(ind[1]):np.max(ind[1]), :]
-    #if np.max(median_gray) != 0:
-    #    pdb.set_trace()
-    #if frame_cropped.shape[0]==0:
     if frame_cropped.shape[0]==0 or frame_cropped.shape[1]==0:
         frame_cropped = np.array([[0, 0],[0, 0]])
-    return frame_output_6, frame_cropped
+    # handle empty frames
+    if np.all(frame_cropped == np.array([[0, 0],[0, 0]])):
+        return frame_output_6, frame_cropped
+    
+    # do dilation
+    kernel =np.ones((8, 2), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
+    #kernel_1 =np.ones((8, 4), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
+    #kernel_2 =np.ones((12, 2), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
+    frame_dilate = cv2.dilate(frame_cropped, kernel)
+    #frame_dilate_erode = cv2.erode(frame_dilate, kernel_2)
+    
+
+    return frame_cropped, frame_dilate
 
