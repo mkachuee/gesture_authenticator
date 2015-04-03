4b6a08ebc14e5c5dc0afd90c8ce6c52c8e98bb19
mehdismh24 <mehdi.smh24@gmail.com>
2015-04-03 16:49:32 -0400

    skin detection added

--- Main.py
+++ Main.py
@@ -42,10 +42,8 @@ while video_capture.isOpened():
         cv2.imshow('output video 1', frame_output_1)
         cv2.imshow('output video 2', frame_output_2)
         # Start of Phase 2
-		frame_justSkin = skindetection.skin_detector(frame_output_1)
-		#frame_justSkin = skindetection.skin_detector(frame_output_2)
-		cv2.imshow('output video 3', frame_justSkin)
+        frame_justSkin = skindetection.skin_detector(frame_output_1)
+        #frame_justSkin = skindetection.skin_detector(frame_output_2)
+        cv2.imshow('output video 3', frame_justSkin)
         cv2.waitKey(int(1000*1.0/VIDEO_FR))
-
-
 
