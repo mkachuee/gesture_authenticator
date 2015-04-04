55e7e3e3e72b3ec9bd04fc9d6a83cf3eebaefaa7
MohamadKachuee <m.kachuee@gmail.com>
2015-04-04 10:42:35 +0430

    Imporved cropping and dilation Addition of crop_point return value

--- Main.py
+++ Main.py
@@ -12,20 +12,22 @@ import cv2
 import background
 # Script options
 VIDEO_SOURCE = \
-    '/mnt/Data/Documents/Courses/SmartEnvironmentVision/Hand_PatternDrawing.avi'
+    "../../SmartVision/Hand_PatternDrawing.avi"
+    #'/mnt/Data/Documents/Courses/SmartEnvironmentVision/Hand_PatternDrawing.avi'
 VIDEO_FR = 30.0
 # Script starts here
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 frame_number = 0
 
 frames_first3s = []
-
+crop_point = (-1, -1)
 while video_capture.isOpened():
     # read from capture and display it
     frame_number += 1
     frame_time = frame_number / VIDEO_FR
     print('frame number: '+ str(frame_number))
     print('frame time: '+ str(frame_time))
+    print('crop_point' + str(crop_point))
     ret, frame_input = video_capture.read()
     cv2.imshow('input video', frame_input)
     # store first 3s frames
@@ -35,12 +37,13 @@ while video_capture.isOpened():
         frame_background = np.uint8(np.mean(frames_first3s, axis=0))
     # process from 3s
     else:
-        frame_output_1, frame_output_2 = background.remove_background(
+        crop_point, frame_output_1, frame_output_2 = \
+            background.remove_background(
             frame_background=frame_background, frame_input=frame_input)
 
         cv2.imshow('output video 1', frame_output_1)
         cv2.imshow('output video 2', frame_output_2)
-        cv2.waitKey(int(1000*1.0/VIDEO_FR))
+        cv2.waitKey(int(1000*0.10/VIDEO_FR))
 
 
 
--- background.py
+++ background.py
@@ -20,35 +20,44 @@ def remove_background(frame_background, frame_input):
         np.abs(np.int_(frame_background_gray) -np.int_(frame_input_gray)))
     # convert to binary image
     ret, frame_output_5 = cv2.threshold(frame_output_4, 64, 255, cv2.THRESH_BINARY)
-    frame_output_6 = np.tile(frame_output_5.transpose()/255, (3, 1, 1)).transpose() * frame_input
-    
 
     # extra precessings
     #frame_output_gaussian = cv2.GaussianBlur(frame_output_6, (3, 3), 0, 0)
     
     # median of image to remove salt and papper noise
-    frame_median = cv2.medianBlur(frame_output_6, 3)
+    frame_median = cv2.medianBlur(frame_output_5, 3)
     
     # crop image
-    median_gray = cv2.cvtColor(frame_median, cv2.COLOR_BGR2GRAY)
+    median_gray = frame_median#cv2.cvtColor(frame_median, cv2.COLOR_BGR2GRAY)
     ind = np.nonzero(median_gray)
     frame_cropped = np.array([[0, 0],[0, 0]])
+    frame_in_cropped = np.array([[0, 0],[0, 0]])
+    crop_point = (-1, -1)
     if len(ind[0]) !=0:
-        frame_cropped = frame_output_6[np.min(ind[0]):np.max(ind[0]), 
+        crop_point = (np.min(ind[0]), np.min(ind[1]))
+        frame_in_cropped = frame_input[np.min(ind[0]):np.max(ind[0]), 
             np.min(ind[1]):np.max(ind[1]), :]
+        frame_cropped = frame_output_5[np.min(ind[0]):np.max(ind[0]), 
+            np.min(ind[1]):np.max(ind[1])]
     if frame_cropped.shape[0]==0 or frame_cropped.shape[1]==0:
         frame_cropped = np.array([[0, 0],[0, 0]])
+        frame_in_cropped = np.array([[0, 0],[0, 0]])
     # handle empty frames
     if np.all(frame_cropped == np.array([[0, 0],[0, 0]])):
-        return frame_output_6, frame_cropped
+        return crop_point, frame_cropped, frame_in_cropped
     
     # do dilation
-    kernel =np.ones((8, 2), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
+    kernel =np.ones((9, 7), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
     #kernel_1 =np.ones((8, 4), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
     #kernel_2 =np.ones((12, 2), np.uint8)# cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,2))
     frame_dilate = cv2.dilate(frame_cropped, kernel)
     #frame_dilate_erode = cv2.erode(frame_dilate, kernel_2)
     
 
-    return frame_cropped, frame_dilate
+    frame_mask = frame_dilate
+
+    frame_output = np.tile(frame_mask.transpose()/255, \
+        (3, 1, 1)).transpose() * frame_in_cropped
+    
+    return crop_point, frame_output, frame_cropped
 
