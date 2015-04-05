e8aa039750e8be9e3888389136c9b9da77dd4930
mehdismh24 <mehdi.smh24@gmail.com>
2015-04-05 17:52:59 -0400

    skin masl pre-process added

--- Main.py
+++ Main.py
@@ -4,52 +4,55 @@ This is the main module of the gesture_authenticator project
 # Imports
 import pdb
 import time
-
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-
 import background
 import skindetection
 # Script options
-VIDEO_SOURCE = \
-    '/mnt/Data/Documents/Courses/SmartEnvironmentVision/Hand_PatternDrawing.avi'
-    #'../../SmartVision/Hand_PatternDrawing.avi'
+VIDEO_SOURCE = 0#\
+#'/home/mehdi/vision/Sample-Video/Hand_PatternDrawing.avi'
+#
 VIDEO_FR = 30.0
 # Script starts here
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 frame_number = 0
 
 frames_first3s = []
-crop_point = (-1, -1)
 while video_capture.isOpened():
-    # read from capture and display it
-    frame_number += 1
-    frame_time = frame_number / VIDEO_FR
-    print('frame number: '+ str(frame_number))
-    print('frame time: '+ str(frame_time))
-    print('crop_point' + str(crop_point))
-    ret, frame_input = video_capture.read()
-    cv2.imshow('input video', frame_input)
-    # store first 3s frames
-    if frame_number < 2:
-        frames_first3s.append(frame_input)
-    elif frame_number == 2:
-        frame_background = np.uint8(np.mean(frames_first3s, axis=0))
-    # process from 3s
-    else:
-        crop_point, frame_output_1, frame_output_2 = \
-            background.remove_background(
-            frame_background=frame_background, frame_input=frame_input)
-        if crop_point[0]==-1 or crop_point[1]==-1:
-            continue
-        cv2.imshow('output video 1', frame_output_1)
-        cv2.imshow('output video 2', frame_output_2)
+	# read from capture and display it
+	frame_number += 1
+	frame_time = frame_number / VIDEO_FR
+	print('frame number: '+ str(frame_number))
+	print('frame time: '+ str(frame_time))
+	ret, frame_input = video_capture.read()
+	cv2.imshow('input video', frame_input)
+	# store first 3s frames
+	if frame_number < 2:
+		frames_first3s.append(frame_input)
+	elif frame_number == 2:
+		frame_background = np.uint8(np.mean(frames_first3s, axis=0))
+	# process from 3s
+	else:
+		crop_point, frame_output_1, frame_output_2 = \
+             		background.remove_background(
+			frame_background=frame_background, frame_input=frame_input)
 
-        cv2.waitKey(int(1000*0.10/VIDEO_FR))
+		
+		if crop_point[0]==-1 or crop_point[1]==-1 or \
+			np.min([frame_output_1.shape[0], frame_output_1.shape[1]])<4:
+			continue
+		cv2.imshow('output video 1', frame_output_1)
+		cv2.imshow('output video 2', frame_output_2)
+		
+		cv2.waitKey(int(1000*0.10/VIDEO_FR))
 
-        # Start of Phase 2
-        #frame_justSkin = skindetection.skin_detector(frame_output_1)
-        frame_justSkin = skindetection.skin_detector(frame_output_1)
-        cv2.imshow('output video 3', frame_justSkin)
-        cv2.waitKey(int(1000*1.0/VIDEO_FR))
+		# Start of Phase 2
+		#frame_justSkin = skindetection.skin_detector(frame_output_1)
+		frame_justSkin = skindetection.skin_detector(frame_output_1)
+		#frame_justSkin = skindetection.skin_detector(frame_output_2)
+		cv2.imshow('output video 3', frame_justSkin)
+		cv2.waitKey(int(500*1.0/VIDEO_FR))
+
+		if cv2.waitKey(1) & 0xFF == ord("q"):
+			break
--- skindetection.py
+++ skindetection.py
@@ -8,18 +8,31 @@ import cv2
 
 # options
 def skin_detector(frame_BGR_input):
-  	"""
-  	This function converts input frame to HSV and detects area of skin
-  	"""
-  	# convert to HSV space
-  	frame_HSV = cv2.cvtColor(frame_BGR_input, cv2.COLOR_BGR2HSV)
-  	cv2.imshow('HSV video', frame_HSV)
-  	# tresholding for find skin
-  	#height, width = frame_HSV.shape[:2]
-  	#frame_skin_detected = np.zeros((height, width, 3), np.uint8)
-  	frame_skin_detected = cv2.inRange(frame_HSV, (0, 70, 60), (20, 160, 255))
-  	#cv2.imshow('skin binary', frame_skin_detected)
-  	frame_BGR_skin = np.tile(frame_skin_detected.transpose()/255, (3, 1, 1)).transpose() * frame_BGR_input
-  	#cv2.imshow('skin detected in BGR', frame_BGR_skin)
-  	
-  	return frame_BGR_skin
+	"""
+	This function converts input frame to HSV and detects area of skin
+	"""
+	# convert to HSV space
+	frame_HSV = cv2.cvtColor(frame_BGR_input, cv2.COLOR_BGR2HSV)
+    	cv2.imshow('HSV video', frame_HSV)
+	# tresholding for find skin
+	#height, width = frame_HSV.shape[:2]
+	#frame_skin_detected = np.zeros((height, width, 3), np.uint8)
+	low_range = np.array([0, 60, 70], dtype="uint8")
+	high_range = np.array([20, 255, 255], dtype="uint8")
+	skin_detected = cv2.inRange(frame_HSV, low_range, high_range)
+
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+
+	#skin_detected = cv2.erode(skin_detected, kernel, iterations=1)
+	skin_detected = cv2.dilate(skin_detected, kernel, iterations=2)
+
+	skin_detected = cv2.GaussianBlur(skin_detected, (3, 3), 0)
+
+	frame_BGR_skin = cv2.bitwise_and(frame_BGR_input, frame_BGR_input, mask=skin_detected)	
+	
+	#cv2.imshow('skin binary', frame_skin_detected)
+	#frame_BGR_skin = np.tile(frame_skin_detected.transpose()/255, (3, 1, 1)).transpose() * frame_BGR_input
+	#cv2.imshow('skin detected in BGR', frame_BGR_skin)
+	
+	return frame_BGR_skin
+
