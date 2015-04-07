757a396162aa7797e97078082372df2a650900ab
MohamadKachuee <m.kachuee@gmail.com>
2015-04-07 23:25:44 +0430

    Improved active hand detection.

--- handdetection.py
+++ handdetection.py
@@ -15,6 +15,10 @@ def find_active_hand(frame_input):
     frame_input_gray = cv2.cvtColor(frame_input, cv2.COLOR_BGR2GRAY)
     frame_bin = cv2.threshold(
         frame_input_gray, 32, 255, cv2.THRESH_BINARY)[1]
+    # do some dilation
+    kernel =np.ones((9, 9), np.uint8)
+    frame_bin = cv2.dilate(frame_bin, kernel)
+    # find contours
     (cnts, _) = cv2.findContours(
         frame_bin.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:3]
@@ -26,11 +30,17 @@ def find_active_hand(frame_input):
     crop_point = (-1, -1)
     if len(cnts) > 1:
         cnt_act = cnts[1]
-        
         # find bounding box
         p1 = np.min(cnt_act, axis=0)
         p2 = np.max(cnt_act, axis=0)
-        #pdb.set_trace()
+        # find active hand
+        if len(cnts) > 2:    
+            p01 = np.min(cnts[2], axis=0)
+            p02 = np.max(cnts[2], axis=0)
+            if p01[0, 1] < p1[0, 1]:
+                p1 = p01
+                p2 = p02
+
         frame_cropped = \
             frame_input[p1[0, 1]:p2[0, 1], p1[0, 0]:p2[0, 0], :]
         crop_point = (p1[0, 0], p1[0, 1])
