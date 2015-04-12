de49441708e94a1626d3feee513a30db8c0ba1ec
mehdismh24 <mehdi.smh24@gmail.com>
2015-04-11 20:18:56 -0400

    skin filter changed

--- skindetection.py
+++ skindetection.py
@@ -25,16 +25,24 @@ def skin_detector(frame_BGR_input, face_rectangles):
 	#frame_skin_detected = np.zeros((height, width, 3), np.uint8)
 	low_range = np.array([0, 60, 70], dtype="uint8")
 	high_range = np.array([20, 255, 255], dtype="uint8")
-	skin_detected = cv2.inRange(frame_HSV, low_range, high_range)
+	skin_detected0 = cv2.inRange(frame_HSV, low_range, high_range)
 	
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+	kernel_v = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+	kernel_h = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 1))
+
+	skin_detected2 = cv2.erode(skin_detected0, kernel_h, iterations=1)
+	skin_detected2 = cv2.dilate(skin_detected2, kernel_v, iterations=1)
+	
+	
+	cv2.imshow('Test', skin_detected2)
 
 	#skin_detected = cv2.erode(skin_detected, kernel, iterations=1)
-	skin_detected = cv2.dilate(skin_detected, kernel, iterations=2)
+	#skin_detected1 = cv2.dilate(skin_detected0, kernel_v, iterations=2)
 
-	skin_detected = cv2.GaussianBlur(skin_detected, (3, 3), 0)
+	#skin_detected1 = cv2.GaussianBlur(skin_detected1, (3, 3), 0)
+	skin_detected2 = cv2.GaussianBlur(skin_detected2, (3, 3), 0)
 
-	frame_BGR_skin = cv2.bitwise_and(frame_BGR_input, frame_BGR_input, mask=skin_detected)	
+	frame_BGR_skin = cv2.bitwise_and(frame_BGR_input, frame_BGR_input, mask=skin_detected2)	
 	
 	#cv2.imshow('skin binary', frame_skin_detected)
 	#frame_BGR_skin = np.tile(frame_skin_detected.transpose()/255, (3, 1, 1)).transpose() * frame_BGR_input
