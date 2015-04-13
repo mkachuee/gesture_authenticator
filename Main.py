e507a0355a0d4357beccafe04cf373adf0d23a0d
MohamadKachuee <m.kachuee@gmail.com>
2015-04-13 10:53:59 +0430

    Basic gesture detector completed

--- Main.py
+++ Main.py
@@ -51,7 +51,8 @@ while video_capture.isOpened():
 		cv2.imshow('output video 2', frame_output_2)
 
 		# face detection , where face_rectangles contain x,y as left upper corner & w,h for width and height
-		face_rectangles = face.face_detect(frame_output_1)
+		"""
+                face_rectangles = face.face_detect(frame_output_1)
 
 		## this part is for test, comment it in application
 		frame_out_face = frame_output_1.copy()
@@ -62,11 +63,12 @@ while video_capture.isOpened():
 		cv2.imshow('output video face', frame_out_face)
                 #pdb.set_trace()
 		##
-
+                """
 		#cv2.waitKey(int(1000*0.10/VIDEO_FR))
 
 		# Start of Phase 2
 		#frame_justSkin = skindetection.skin_detector(frame_output_1)
+                face_rectangles = -1
 		frame_justSkin = skindetection.skin_detector(frame_output_1, face_rectangles)
 		cv2.imshow('output video 3', frame_justSkin)
                 
@@ -75,13 +77,15 @@ while video_capture.isOpened():
                     handdetection.find_active_hand(frame_justSkin)
                 
                 # find hand gesture
-                frame_gesture =  handgesture.detect_gesture(frame_hand)
+                frame_gesture, est_gesture = \
+                    handgesture.detect_gesture(frame_hand)
+                print(est_gesture)
 
                 if hand_pos != (-1, -1):
                     cv2.imshow('output video 4', frame_contours)
 		    cv2.imshow('output video 5', frame_hand)
 		    cv2.imshow('output video 6', frame_gesture)
-		    cv2.waitKey(int(500*1.0/VIDEO_FR))
+		    #cv2.waitKey(int(500*1.0/VIDEO_FR))
 		#press q for breaking the loop
 		if cv2.waitKey(1) & 0xFF == ord("q"):
 			break
--- handgesture.py
+++ handgesture.py
@@ -9,8 +9,9 @@ import cv2
 
 def detect_gesture(input_frame):
     # check the input
+    est_gesture = -1
     if input_frame.shape[0] < 4:
-        return input_frame
+        return input_frame, est_gesture
     # convert to binary
     frame_gray = cv2.cvtColor(input_frame, cv2.COLOR_BGR2GRAY)
     _, frame_bin = cv2.threshold(frame_gray, 32, 255, cv2.THRESH_BINARY)
@@ -18,6 +19,7 @@ def detect_gesture(input_frame):
     contours, hierarchy = cv2.findContours(
         frame_bin.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # find the largest contour
+
     if len(contours) != 0:
         cnt = sorted(contours, key = cv2.contourArea, reverse = True)[0]
         # draw the convex hull
@@ -25,7 +27,7 @@ def detect_gesture(input_frame):
         cv2.drawContours(input_frame, [cnt], -1, (0,255,0), 2)
         cv2.drawContours(input_frame, [hull], -1, (0,0,255), 2)
         # find convexity defects
-        cnt = cv2.approxPolyDP(cnt,0.02*cv2.arcLength(cnt,True),True)
+        cnt = cv2.approxPolyDP(cnt,0.03*cv2.arcLength(cnt,True),True)
         hull = cv2.convexHull(cnt,returnPoints = False)
         defects = np.zeros((0, 0))
         if hull.shape[0] > 3:
@@ -40,9 +42,24 @@ def detect_gesture(input_frame):
                 #dist = cv2.pointPolygonTest(cnt,centr,True)
                 cv2.line(input_frame,start,end,[0,255,0],2)
                 cv2.circle(input_frame,far,5,[0,0,255],-1)
-                print(i)
+            #print(i+1)
+            print(input_frame.shape)
+            # estimate hand gesture
+            num_points = defects.shape[0]
+            est_gesture = -1
+            if num_points == 0:
+                est_gesture = 3
+            elif num_points <= 2:
+                #pdb.set_trace()
+                #print(cnt[defects[0, 0][2]][0][1])
+                if cnt[defects[0, 0][2]][0][1] > 20 and \
+                        np.abs(input_frame.shape[0]-input_frame.shape[1]) > 20:
+                    est_gesture = 2
+            elif num_points > 3:
+                est_gesture=1
+                
         except:
             print(-1)
 
 
-    return frame_bin
+    return frame_bin, est_gesture
